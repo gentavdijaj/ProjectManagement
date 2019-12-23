490c31ca8459bb7695466009bd5c8e2b735c62c6
root <root@localhost.localdomain>
2019-12-23 15:57:03 +0100

    first-commit

---
+++ ProjectManagement/src/main/java/com/gent/projectmanagement/repositories/ProjectRepository.java
@@ -0,0 +1,15 @@
+package com.gent.projectmanagement.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gent.projectmanagement.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long>{
+
+	@Override
+	 Iterable<Project> findAllById(Iterable<Long> iterable);
+	
+	
+}
